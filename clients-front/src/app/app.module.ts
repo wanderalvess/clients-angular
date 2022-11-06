5ce1afa630320836f3110dfbb764799ea6639cd9
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2022-11-05 21:19:13 -0300

    Chave Maps

--- clients-front/src/app/app.module.ts
+++ clients-front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,10 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCkoq1g2Gv3SOZDJ7k1V-rORPS0aRfPo8g'
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
