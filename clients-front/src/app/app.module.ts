3f06b2fbc7c21f626760aacbdb42ab85e9dd266a
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2022-11-06 17:41:43 -0300

    Update package.json

--- clients-front/src/app/app.module.ts
+++ clients-front/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,9 +17,6 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     HttpClientModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCkoq1g2Gv3SOZDJ7k1V-rORPS0aRfPo8g'
-    })
 
   ],
   providers: [],
